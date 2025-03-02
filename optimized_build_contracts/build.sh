f527fe56b841dcee1d2f17301272d2bc93646596
The SunShining <thesunshiningmin@gmail.com>
2025-03-02 13:45:54 -0700

    slightly more size optimized!

--- optimized_build_contracts/build.sh
+++ optimized_build_contracts/build.sh
@@ -3,12 +3,14 @@ set -e
 
 pushd token
 ./build.sh
-wasm-opt res/fungible_token.wasm -O4 -o res/fungible_token.wasm
+wasm-opt res/fungible_token.wasm -Oz -o res/fungible_token.wasm
+wasm-strip res/fungible_token.wasm
 popd
 
 pushd factory
 ./build.sh
-wasm-opt res/token_factory.wasm -O4 -o res/token_factory.wasm
+wasm-opt res/token_factory.wasm -Oz -o res/token_factory.wasm
+wasm-strip res/token_factory.wasm
 popd
 
 
