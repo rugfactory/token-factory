479bc6ade51e756625aa45215d5a17e17fa34fd9
The SunShining <thesunshiningmin@gmail.com>
2025-03-02 13:50:13 -0700

    more size optimization?

--- optimized_build_contracts/factory/build.sh
+++ optimized_build_contracts/factory/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+RUSTFLAGS='-C opt-level=z -C codegen-units=1 -C panic=abort -C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/token_factory.wasm ./res/
+wasm-opt ./res/token_factory.wasm -Oz -o ./res/token_factory.wasm
+wasm-strip ./res/token_factory.wasm
 
--- optimized_build_contracts/token/build.sh
+++ optimized_build_contracts/token/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+RUSTFLAGS='-C opt-level=z -C codegen-units=1 -C panic=abort -C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/fungible_token.wasm ./res/
+wasm-opt ./res/fungible_token.wasm -Oz -o ./res/fungible_token.wasm
+wasm-strip ./res/fungible_token.wasm
 
