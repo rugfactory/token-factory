186df20401b286a01e14fe2d5a0ced69b5daf888
The SunShining <thesunshiningmin@gmail.com>
2025-03-04 17:42:57 -0700

    build op

--- optimized_build_contracts/token/build.sh
+++ optimized_build_contracts/token/build.sh
@@ -3,6 +3,3 @@ set -e
 
 RUSTFLAGS='-C opt-level=z -C codegen-units=1 -C panic=abort -C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/fungible_token.wasm ./res/
-wasm-opt ./res/fungible_token.wasm -Oz -o ./res/fungible_token.wasm
-wasm-strip ./res/fungible_token.wasm
-
